3ad68b83a7dca17842c42dab204788cd5c85eaca
Oikaze <102778036+oikazeHiro@users.noreply.github.com>
2024-04-30 17:00:09 +0800

    一些常用依赖

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,11 +30,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
@@ -66,4 +66,25 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    // https://mvnrepository.com/artifact/io.insert-koin/koin-androidx-compose
+    // koin 依赖注入框架
+    implementation(libs.koin.androidx.compose)
+    // https://mvnrepository.com/artifact/com.squareup.okhttp3/okhttp
+    // okhttp
+    implementation(libs.okhttp)
+    // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-serialization-json
+    // json
+    implementation(libs.kotlinx.serialization.json)
+    // 这个库提供了一组类似于可组合项的设置来帮助android Jetpack组合 开发人员构建复杂的设置屏幕，而无需所有样板文件。
+    implementation(libs.github.ui.tiles)
+    // https://mvnrepository.com/artifact/com.squareup.retrofit2/retrofit
+    // retrofit
+    implementation(libs.squareup.retrofit)
+
+    // room 使用 Room 持久保留数据
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.paging)
+    implementation(libs.androidx.room.ktx)
+    implementation (libs.androidx.room.room.compiler2)
+
 }
