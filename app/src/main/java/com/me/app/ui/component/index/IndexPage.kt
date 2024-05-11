76e0d5379fcf64805b1ed03081b8ba8af920e2b0
Oikaze <102778036+oikazeHiro@users.noreply.github.com>
2024-05-11 14:30:26 +0800

    测试

--- app/src/main/java/com/me/app/ui/component/index/IndexPage.kt
+++ app/src/main/java/com/me/app/ui/component/index/IndexPage.kt
@@ -6,52 +6,54 @@ import androidx.compose.material.icons.automirrored.filled.Message
 import androidx.compose.material.icons.filled.Contacts
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.stringResource
 import com.me.app.R
-import com.me.app.ui.component.index.layout.ContactDestination
-import com.me.app.ui.component.index.layout.HomeDestination
-import com.me.app.ui.component.index.layout.MeDestination
-import com.me.app.ui.component.index.layout.MessageDestination
 
 @Composable
 fun IndexPage(){
     var currentDestination by rememberSaveable { mutableStateOf(AppDestinations.HOME) }
-    NavigationSuiteScaffold(
-        navigationSuiteItems = {
-            AppDestinations.entries.forEach {
-                item(
-                    icon = {
-                        Icon(
-                            it.icon,
-                            contentDescription = stringResource(it.contentDescription)
-                        )
-                    },
-                    label = { Text(stringResource(it.label)) },
-                    selected = it == currentDestination,
-                    onClick = { currentDestination = it }
-                )
-            }
-        },
-        containerColor = MaterialTheme.colorScheme.primary,
-        contentColor = MaterialTheme.colorScheme.onPrimary,
-    ) {
-        when (currentDestination) {
-            AppDestinations.HOME -> HomeDestination()
-            AppDestinations.CONTACT -> ContactDestination()
-            AppDestinations.MESSAGE -> MessageDestination()
-            AppDestinations.ME -> MeDestination()
+//    NavigationSuiteScaffold(
+//        navigationSuiteItems = {
+//            AppDestinations.entries.forEach {
+//                item(
+//                    icon = {
+//                        Icon(
+//                            it.icon,
+//                            contentDescription = stringResource(it.contentDescription)
+//                        )
+//                    },
+//                    label = { Text(stringResource(it.label)) },
+//                    selected = it == currentDestination,
+//                    onClick = { currentDestination = it }
+//                )
+//            }
+//        },
+//        containerColor = MaterialTheme.colorScheme.primary,
+//        contentColor = MaterialTheme.colorScheme.onPrimary,
+//    ) {
+//        when (currentDestination) {
+//            AppDestinations.HOME -> HomeDestination()
+//            AppDestinations.CONTACT -> ContactDestination()
+//            AppDestinations.MESSAGE -> MessageDestination()
+//            AppDestinations.ME -> MeDestination()
+//        }
+//    }
+
+    Scaffold(
+        bottomBar ={
+            NavigationBar
         }
+    ){
+
     }
+
 }
 
 enum class AppDestinations(
